7f207e7656eb5adae238108e8d180c3df3261244
ShinHyungJune <ssa4141@naver.com>
2018-12-25 17:21:41 +0900

    모델팩토리, 테스팅 db 연결

--- config/database.php
+++ config/database.php
@@ -56,6 +56,22 @@ return [
             'engine' => null,
         ],
 
+        'mysql_testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_TEST', '127.0.0.1'),
+            'port' => env('DB_PORT_TEST', '3306'),
+            'database' => env('DB_DATABASE_TEST', 'forge_test'),
+            'username' => env('DB_USERNAME_TEST', 'forge_test'),
+            'password' => env('DB_PASSWORD_TEST', ''),
+            'unix_socket' => env('DB_SOCKET_TEST', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
